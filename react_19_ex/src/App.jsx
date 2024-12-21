f6c7ff27a0260f12a2fd761c793eab73e8bee766
sychoi <cinnamon1995@naver.com>
2024-12-21 16:21:56 +0900

    useUpdateState => 말그대로 state 를 업데이트 쳐주는 함수로 useState처럼 디폴트 지정도 가능하고, isPending 처리도 가능하다

--- react_19_ex/src/App.jsx
+++ react_19_ex/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Use from './components/use/Use.jsx';
 import Action from './components/action/Action.jsx';
+import UseActionState from './components/useActionState/UseActionState.jsx';
 
 function App() {
 
   return (
     <>
-      <Action/>
+      {/*<Action/>*/}
+      <UseActionState />
     </>
   )
 }
--- react_19_ex/src/components/useActionState/UseActionState.jsx
+++ react_19_ex/src/components/useActionState/UseActionState.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React,{useActionState} from 'react';
 
+async function updateName (name){
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+  return name
+}
 const UseActionState = () => {
+  const [name,updateNameAction, isPending ] =  useActionState(
+    async (previousName, formData) => {
+      const newName = await updateName(formData.get('name'))
+      return newName
+    },
+    ''
+  )
   return (
-    <div>UseActionState</div>
+    <form action={updateNameAction}>
+      Current name is {name}
+      <input name='name' />
+      <button type='submit' disabled={isPending}>
+        {isPending ?'Updating...': 'Update'}
+      </button>
+    </form>
   )
 }
 
